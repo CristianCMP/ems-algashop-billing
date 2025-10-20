353ce5072c11c91f3d9ed10215f0f37144dc9984
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-20 14:00:52 -0300

    - Using the JavaBean Pattern Safely

--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/Invoice.java
@@ -1,13 +1,18 @@
 package com.algaworks.algashop.billing.domain.model.invoice;
 
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+@Setter(AccessLevel.PRIVATE)
+@Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Invoice {
 
     @EqualsAndHashCode.Include
@@ -32,4 +37,19 @@ public class Invoice {
 
     private String cancelReason;
 
+    public Set<LineItem> items() {
+        return Collections.unmodifiableSet(items);
+    }
+
+    public void markAsPaid() {
+    }
+
+    public void cancel() {
+    }
+
+    public void assignPaymentGatewayCode(String code) {
+    }
+
+    public void changePaymentSettings(PaymentMethod method, UUID creditCard) {
+    }
 }
