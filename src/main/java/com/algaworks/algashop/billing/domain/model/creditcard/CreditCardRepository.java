491aca0ce3367fe0b90556af7d14bddd33531378
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-10-21 10:41:34 -0300

    - Persistence-oriented repository

---
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
@@ -0,0 +1,6 @@
+package com.algaworks.algashop.billing.domain.model.creditcard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
+}
