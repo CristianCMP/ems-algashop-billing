965cd2e010fa78ec2723eca83fa9542dd82c5f3e
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-08-06 21:20:50 -0300

    - Challenge: Enhance audit capabilities

---
+++ src/main/java/com/algaworks/algashop/billing/application/security/SecurityCheckApplicationService.java
@@ -0,0 +1,12 @@
+package com.algaworks.algashop.billing.application.security;
+
+import java.util.UUID;
+
+public interface SecurityCheckApplicationService {
+    UUID getAuthenticatedUserId();
+
+    boolean isAuthenticated();
+
+    boolean isMachineAuthenticated();
+    boolean canAccessOwnProfile();
+}
