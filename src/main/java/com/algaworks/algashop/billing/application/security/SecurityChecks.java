b191a903fd1121fa1ec1e9c9429e9cdbab429f94
Cristian Puhl <cristian.puhl@universo.univates.br>
2026-08-12 09:53:14 -0300

    - Tests to ensure security rules

--- src/main/java/com/algaworks/algashop/billing/application/security/SecurityChecks.java
+++ src/main/java/com/algaworks/algashop/billing/application/security/SecurityChecks.java
@@ -2,11 +2,8 @@ package com.algaworks.algashop.billing.application.security;
 
 import java.util.UUID;
 
-public interface SecurityCheckApplicationService {
+public interface SecurityChecks {
     UUID getAuthenticatedUserId();
-
     boolean isAuthenticated();
-
     boolean isMachineAuthenticated();
-    boolean canAccessOwnProfile();
 }
